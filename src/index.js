5ef27af0091448100b69e8f0bdb6f3b2cc2784ab
annconceivable <84342516+annconceivable@users.noreply.github.com>
2022-01-14 21:39:09 +0300

    init: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  let closeBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i ++) {
+    brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    closeBrackets.push( bracketsConfig[i][1] );
+  }
+  let stack = [];
+
+  const returnKey = (obj, value) => {
+    for (key in obj) {
+      if (obj[key] === value) return key;
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    if (closeBrackets.includes(currentSymbol)) {
+      if (stack[stack.length - 1] === returnKey(brackets, currentSymbol)) {
+        stack.pop();
+      }
+      else stack.push(currentSymbol);
+    }
+    else {
+      stack.push(currentSymbol);
+    }
+  }
+  
+
+  /*
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    
+    
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0 ) {
+          return false;
+        }
+
+      let topElement = stack[stack.length - 1];
+              if (brackets[currentSymbol] === topElement) {
+                stack.pop();
+              }
+      else {
+      return false;
+      }
+    }
+  }
+
+  */
+  return ( stack.length === 0 );
 }
